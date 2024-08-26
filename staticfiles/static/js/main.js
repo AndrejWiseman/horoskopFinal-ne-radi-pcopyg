a30391bdf457246b4e5372ec21bd8c9497f3faf3
Andrej <533737@gmail.com>
2024-08-26 17:44:41 +0200

    env

---
+++ staticfiles/static/js/main.js
@@ -0,0 +1,9 @@
+
+function showSpinner() {
+    document.getElementById("spinner-overlay").style.display = "block";
+}
+
+function hideSpinner() {
+    document.getElementById("spinner-overlay").style.display = "none";
+}
+
